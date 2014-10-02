c9670f2891e314920a3b41164f6caa75dc3b944f
ITSPaul <powell.paul@itsligo.ie>
2014-10-02 15:51:22 +0100

    Initial Hub Code added

---
+++ SignalrGameAzServer/MessageTypes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MessageTypes
+{
+    public class myPoint
+    {
+        public float X;
+        public float Y;
+
+        public myPoint(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class myPlayer
+    {
+        myPoint pos;
+
+        public myPoint Pos
+        {
+            get { return pos; }
+            set { pos = value; }
+        }
+        int playerID;
+
+        public int PlayerID
+        {
+            get { return playerID; }
+            set { playerID = value; }
+        }
+
+        public myPlayer(int playerid, myPoint p)
+        { playerID = playerid; pos = p; }
+    }
+
+}
---
+++ SignalrGameAzServer/gameServerHub.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using MessageTypes;
+using Microsoft.AspNet.SignalR.Hubs;
+
+
+namespace SignalrGameAzServer
+{
+    [HubName("paspAzureGameHub")]
+    public class gameServerHub : Hub
+    {
+        static Dictionary<int, myPlayer> _players = new Dictionary<int, myPlayer>();
+        static int _playerCount = 0;
+
+        public void SendMessage(string message)
+        {
+            Clients.All.UpdateMessageR(message);
+        }
+
+        public void JoinServer(myPoint pos)
+        {
+            int id = ++_playerCount;
+            myPlayer p =
+                new myPlayer(id, pos);
+            Clients.Caller.JoinedServer(p);
+            Clients.Others.AddOtherPlayer(p);
+            foreach (KeyValuePair<int, myPlayer> player in _players)
+            {
+                Clients.Caller.AddOtherPlayer(player.Value);
+            }
+            _players.Add(id, p);
+        }
+
+        public void UpdatePlayerPosition(myPlayer p)
+        {
+            Clients.Others.UpdatePosition(p);
+            _players[p.PlayerID].Pos = p.Pos;
+        }
+
+        public void DeletePlayer(myPlayer p)
+        {
+            _players.Remove(p.PlayerID);
+            Clients.Others.removePlayer(p);
+        }
+    }
+}
